e7293a1c6a6b1c3f10977fff2b03048b93f27725
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-10-16 20:23:49 +0100

    url change

--- src/Axios/ServerApi.js
+++ src/Axios/ServerApi.js
@@ -5,7 +5,7 @@ axios.defaults.withCredentials = true;
 // for local hosting : baseURL: "http://localhost:3005/",
 
 const ServerApi = axios.create({
-  baseURL: "server.espanolmaestro.com/",
+  baseURL: "http://www.server.espanolmaestro.com/",
   withCredentials: true,
 });
 
